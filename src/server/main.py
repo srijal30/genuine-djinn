d2611a61231e3a6084ff431a9066619176e60d70
ZeroIntensity <zintensitydev@gmail.com>
2022-07-21 19:50:57 -0400

    ok

--- src/server/main.py
+++ src/server/main.py
@@ -3,11 +3,13 @@ import uvicorn
 
 app = FastAPI()
 
-@app.websocket('/')
+
+@app.websocket("/")
 async def socket(ws: WebSocket):
     await ws.accept()
-    await ws.send("hello world")
+    await ws.send_text("hello world")
     await ws.close()
 
+
 if __name__ == "__main__":
-    uvicorn.run(app, host = '0.0.0.0', host = 5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
