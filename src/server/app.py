44c9c5de671cfd7fbc190d80a96d25158ec661ae
ZeroIntensity <zintensitydev@gmail.com>
2022-07-22 13:59:47 -0400

    do imports correctly challenge (impossible)

--- src/server/app.py
+++ src/server/app.py
@@ -1,11 +1,9 @@
-import uvicorn  # type: ignore
 from fastapi import FastAPI
 
 from .socket_router import router
 
+__all__ = ("app",)
+
 app = FastAPI()
 
 app.include_router(router)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5000)
