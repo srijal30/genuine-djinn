98e6aca5ba8f6d52600d823408efab8497c6d98b
bobbyinvents <72687709+bobbyinvents@users.noreply.github.com>
2022-07-28 15:32:17 -0400

    Move to main.py

--- client/enhancers/message_processer.py
+++ client/enhancers/message_processer.py
@@ -39,12 +39,3 @@ class MessageProcesser:
     def autocorrect(self, message: str):
         """Autocorrect message."""
         ...
-
-
-if __name__ == "__main__":
-    m = MessageProcesser()
-    m.add_new_message("Hello, world! How are you?")
-    m.add_new_message("I dropped my spaghetti.")
-    m.add_new_message("Linux is better than Windows and Mac.")
-    print(m.messages)
-    print(m.translations)
