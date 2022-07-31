cf8d8fd3c31064f8adf045a9d28bb0403fea046f
bobbyinvents <72687709+bobbyinvents@users.noreply.github.com>
2022-07-31 05:26:01 -0400

    Delete unnecessary code

--- client/enhancers/message_processer.py
+++ client/enhancers/message_processer.py
@@ -1,15 +1,12 @@
 import random
 import re
 import string
-# from collections import defaultdict
 from pathlib import Path
 
 import spacy
 from spacy.matcher import Matcher
 
 from .translations import boomhauer, emojify, owoify, pig_latin
-
-# from typing import Dict
 
 
 class AutoTranslater:
@@ -22,11 +19,6 @@ class AutoTranslater:
 
         # Initialize the matcher with the shared vocab
         self.matcher: spacy.matcher.Matcher = Matcher(self.nlp.vocab)
-
-        # DELETE THIS AFTER TESTING
-        # # Initialize a dictionary of messages to their processed texts
-        # self.messages: Dict[int, spacy.tokens.doc.Doc] = {}
-        # self.translated_messages: Dict[int, Dict[str, str]] = defaultdict(dict)
 
         # List of translations
         self.translations = [
@@ -45,30 +37,6 @@ class AutoTranslater:
             # self.auto_translate_to_emojify.__name__: 1,  # Needs emoji support
             self.auto_translate_to_pig_latin.__name__: 1,
         }
-
-    # DELETE THIS AFTER TESTING
-    # def add_new_message(self, message: str):
-    #     """Add a new message to the NLP processer."""
-    #     doc: spacy.tokens.doc.Doc = self.nlp(message)
-    #     message_hash = hash(message)
-    #     self.messages[message_hash] = doc
-    #     self.auto_translate(doc, message, message_hash)
-
-    # DELETE THIS AFTER TESTING
-    # def auto_translate(
-    #     self, doc: spacy.tokens.doc.Doc, message: str, message_hash: int
-    # ):
-    #     """Translate message."""
-    #     self.translated_messages[message_hash][
-    #         boomhauer.__name__
-    #     ] = boomhauer.boomhauer(doc)
-    #     self.translated_messages[message_hash][emojify.__name__] = emojify.emojify(doc)
-    #     self.translated_messages[message_hash][owoify.__name__] = owoify.owoify(
-    #         self.nlp, doc
-    #     )
-    #     self.translated_messages[message_hash][
-    #         pig_latin.__name__
-    #     ] = pig_latin.pig_latin(doc)
 
     def random_autotranslate(self, message: str):
         """Choose a random auto-translate method using probability weights."""
@@ -179,12 +147,6 @@ class AutoCorrecter:
         )
         return random_autocorrect_method(message)
 
-    # DELETE THIS AFTER TESTING
-    # def autocorrect_string(self, message: str) -> str:
-    #     """Autocorrect message by manipulating the string."""
-    #     random_obscuring_method = random.choice(self.autocorrect_methods)
-    #     return random_obscuring_method(message)
-
     def autocorrect_entities(self, message: str) -> str:
         """Autocorrect message by replacing entities."""
         doc: spacy.tokens.doc.Doc = self.nlp(message)
@@ -192,8 +154,6 @@ class AutoCorrecter:
 
         # Replace with entities labels for now.
         for ent in doc.ents:
-            # print(ent.text, ent.start, ent.end, ent.label_, spacy.explain(ent.label_))
-            # token_list[ent.start: ent.end] = [f"\u007b{ent.label_}\u007d"]
             random_entity = self._get_random_entity(ent.label_, ent.text)
             token_list[ent.start: ent.end] = [random_entity]
 
